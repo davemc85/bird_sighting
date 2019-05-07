04052ce2b66ec6498104e82aec041869197dc8ce
davemc85 <davemcallister@hotmail.co.uk>
2019-05-07 18:01:30 +0100

    first commit

---
+++ client/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <sightings-form />
+    <sightings-grid :sightings="sightings" />
+  </div>
+</template>
+
+<script>
+import SightingsForm from './components/SightingsForm';
+import SightingsGrid from './components/SightingsGrid';
+import { eventBus } from './main';
+import BirdSightingService from '@/services/BirdSightingService';
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      sightings: []
+    }
+  },
+  components: {
+    SightingsForm,
+    SightingsGrid
+  },
+  mounted(){
+    this.fetchData();
+
+    eventBus.$on('refresh-data', this.fetchData);
+  },
+  methods: {
+    fetchData(){
+      fetch("http://localhost:3000/api/sightings")
+        .then(res => res.json())
+        .then(sightings => this.sightings = sightings);
+    }
+  }
+}
+</script>
+
+<style>
+html {
+  height: 100%;
+}
+
+body {
+  background: url('./assets/birds-background.jpg') no-repeat;
+  height: 100%;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+
+}
+</style>
